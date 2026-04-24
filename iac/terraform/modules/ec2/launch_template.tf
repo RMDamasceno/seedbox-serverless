c8e5befbcea8d1ee3496e5dcf362712c166ce190
Rafael Damasceno <contato@rafael.damasceno.nom.br>
2026-04-24 03:58:58 -0300

    fix: filtro AMI para Amazon Linux 2023 standard (excluir ECS/minimal)

--- iac/terraform/modules/ec2/launch_template.tf
+++ iac/terraform/modules/ec2/launch_template.tf
@@ -4,12 +4,17 @@ data "aws_ami" "amazon_linux_2023" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["al2023-ami-2023.*-kernel-*-x86_64"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
